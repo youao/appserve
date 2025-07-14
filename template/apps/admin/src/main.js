cea64a877b4b8a94476959891711ce4a56bdd648
aodada <aodada2588@163.com>
2025-07-14 16:53:55 +0800

    refactor: 共享ui

--- template/apps/admin/src/main.js
+++ template/apps/admin/src/main.js
@@ -1,5 +1,4 @@
 import "virtual:uno.css";
-import "@pkg/ui/dist/ui.css";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
--- template/packages/ui/src/index.js
+++ template/packages/ui/src/index.js
@@ -1,5 +1,3 @@
-import "virtual:uno.css";
-
 import Button from "./button/Button.vue";
 
 export { Button };
